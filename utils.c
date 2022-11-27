828882594a55b1587a997948ed6c41261f9fef07
Tommaso Severi <tommaso.severi2@studio.unibo.it>
2022-11-27 17:23:43 +0100

    Implemented Input functions

--- utils.c
+++ utils.c
@@ -12,3 +12,36 @@
 #include <time.h>
 #include <string.h>
 #include "utils.h"
+
+// --------INPUT FUNCTIONS--------
+
+void stralloc(char** str){
+    char* strtmp = (char*)malloc(sizeof(char) * MAX_STRING_SIZE);
+    scanf("%s", strtmp);
+    *str = (char*)malloc(sizeof(char) * strlen(strtmp) + 1);
+    if(str == NULL){
+        exit(1);
+    }
+    strcpy(*str, strtmp);
+    free(strtmp);
+}
+
+void fstralloc(char** str, FILE* file){
+    char* strtmp = (char*)malloc(sizeof(char) * MAX_STRING_SIZE);
+    fgets(strtmp, MAX_STRING_SIZE, file);
+    int lung = strlen(strtmp);
+    strtmp[lung]='\0';
+    *str = (char*)malloc(sizeof(char) * lung + 1);
+    if(str == NULL){
+        exit(1);
+    }
+    strcpy(*str, strtmp);
+    free(strtmp);
+}
+
+void flushStdin(){
+    int c;
+    do {
+        c = getchar();
+    } while(c != '\n' && c != EOF);
+}
