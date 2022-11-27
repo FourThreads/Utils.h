779067fb79336d5609075c9fc10ee5dc34c8ab87
Tommaso Severi <tommaso.severi2@studio.unibo.it>
2022-11-27 17:22:54 +0100

    Started the implementation

---
+++ utils.c
@@ -0,0 +1,14 @@
+/*
+ * STUDENTS:    Buda Francesco, Ceredi Tommaso, Maglia Danilo, Severi Tommaso
+ * DATE:    	15/11/2021
+ * FILE:        utils.c
+ * DESCRIPTION: IMPLEMENTATION OF LIBRARY "utils". 
+ *              FOR DEFINITION SEE "utils.h"
+ * VERSIONE:    1.5
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
+#include "utils.h"
